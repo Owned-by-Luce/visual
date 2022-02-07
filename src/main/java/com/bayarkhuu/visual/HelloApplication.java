a0aa41ae8561252b425c27ed2d99e89a65084a68
bayarkhuu123 <bayarkhuu.srs@gmail.com>
2022-02-07 14:16:14 +0800

    Initial commit

---
+++ src/main/java/com/bayarkhuu/visual/HelloApplication.java
@@ -0,0 +1,47 @@
+package com.bayarkhuu.visual;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        //Ташуу зураас
+        root.getChildren().add(new Line(0, 0, 450, 450));
+
+        //Том дөрвөлжин
+        Rectangle rectangle = new Rectangle(0, 0, 450, 450);
+        rectangle.setStroke(Color.BLACK);
+        rectangle.setFill(Color.TRANSPARENT);
+        root.getChildren().add(rectangle);
+
+        //Жижиг дөрвөлжин
+        Rectangle rectangleSmall = new Rectangle(0, 0, 400, 400);
+        rectangleSmall.setStroke(Color.BLACK);
+        rectangleSmall.setFill(Color.TRANSPARENT);
+        root.getChildren().add(rectangleSmall);
+
+        //Тойрог
+        Circle circle = new Circle(150, 150, 100);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.TRANSPARENT);
+        root.getChildren().add(circle);
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
+
+        primaryStage.show();
+    }
+}
