adf42e32c072c9dfdec57829f47decd4ee581cdc
owned-by-luce <bayarkhuu.srs@gmail.com>
2022-02-21 14:15:23 +0800

    2022.02.21 lab3

--- src/main/java/com/bayarkhuu/visual/lab3/Lab3App.java
+++ src/main/java/com/bayarkhuu/visual/lab3/Lab3App.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Lab3App extends Application {
