fe9816ddaee100a9021d70ff8d7ab2b619d0e5fd
owned-by-luce <bayarkhuu.srs@gmail.com>
2022-02-28 15:01:48 +0800

    2022.02.28

--- src/main/java/com/bayarkhuu/visual/labs/lab4/AnimalController.java
+++ src/main/java/com/bayarkhuu/visual/labs/lab4/AnimalController.java
@@ -2,13 +2,9 @@ package com.bayarkhuu.visual.labs.lab4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Labeled;
+import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 public class AnimalController {
     @FXML
@@ -16,14 +12,6 @@ public class AnimalController {
 
     @FXML
     protected void change(ActionEvent e) {
-        InputStream inputStream;
-        try {
-            inputStream = new FileInputStream("F:/" + ((Labeled) e.getSource()).getText() + ".gif");
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-            return;
-        }
-        Image image = new Image(inputStream);
-        imageView.setImage(image);
+        imageView.setImage(new Image("F:/" + ((RadioButton) e.getSource()).getText() + ".gif"));
     }
 }
